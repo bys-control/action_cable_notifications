ed731a20678e289f7ae5c03368ff32e34bfb22cd
Guillermo Bisheimer <gbisheimer@bys-control.com.ar>
2016-09-04 19:45:50 -0300

    Sets broadcast options if they are not already present in the model Fix clearing options on unsubscribe

--- lib/action_cable_notifications/streams.rb
+++ lib/action_cable_notifications/streams.rb
@@ -9,10 +9,8 @@ module ActionCableNotifications
     private
 
     def stream_notifications_for(model, options = {}, &block)
-      @model = model
-
       # Default options
-      @options = {
+      options = {
         actions: [:create, :update, :destroy],
         broadcasting: model.model_name.collection,
         callback: nil,
@@ -23,29 +21,29 @@ module ActionCableNotifications
         }.merge(options)
 
       # Checks if model already includes notification callbacks
-      if !@model.respond_to? :ActionCableNotificationsOptions
-        @model.send('include', ActionCableNotifications::Callbacks)
+      if !model.respond_to? :ActionCableNotificationsOptions
+        model.send('include', ActionCableNotifications::Callbacks)
+        # Set specified options on model
       end
 
-      # Set specified options on model
-      @model.send('action_cable_notification_options=', @options[:broadcasting], @options)
+      # Sets broadcast options if they are not already present in the model
+      if not model.ActionCableNotificationsOptions.key? options[:broadcasting]
+        model.broadcast_notifications_from options[:broadcasting], options
+      end
 
       # Start streaming
-      stream_from(@options[:broadcasting], options[:callback] || block, @options.slice(:coder))
+      stream_from(options[:broadcasting], options[:callback] || block, options.slice(:coder))
 
       # Transmit initial state if required
-      if @options[:include_initial]
+      if options[:include_initial]
         # XXX: Check if data should be transmitted
-        transmit @model.notify_initial @options[:broadcasting]
+        transmit model.notify_initial options[:broadcasting]
       end
 
     end
 
     def unsubscribed
       stop_all_streams
-
-      # Unset options for this channel on model
-      @model.send('action_cable_notification_options=', @options[:broadcasting], nil)
     end
 
   end
