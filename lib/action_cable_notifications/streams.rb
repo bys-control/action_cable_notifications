14e58e7ba70f7759f4d4b68489b8ebd7800decd4
Guillermo Bisheimer <gbisheimer@bys-control.com.ar>
2016-09-30 08:05:16 -0300

    Adds server side functions to be called using perform

--- lib/action_cable_notifications/streams.rb
+++ lib/action_cable_notifications/streams.rb
@@ -7,9 +7,96 @@ module ActionCableNotifications
       # Actions to be done when the module is included
     end
 
-    def get_initial_values
-      transmit self.stream_notification_options[:model].notify_initial self.stream_notification_options[:broadcasting]
+    #
+    # Methods to be called from the client with perform()
+    ################################################################
+
+    #
+    # Fetch records from the DB and send them to the client
+    #
+    # @param [Hash] selector Specifies conditions that the registers should match
+    # @param [Hash] options Options
+    #
+    def fetch(data)
+      # XXX: Check if the client is allowed to call the method
+
+      # Remove action name from data
+      data.except!("action")
+
+      # Record fetchnig options
+      options = {
+        limit: data["limit"],
+        where: data["where"] || {},
+        fields: data["fields"]
+      }
+
+      model = self.stream_notification_options[:model]
+      broadcasting = self.stream_notification_options[:broadcasting]
+      broadcasting_options = model.ActionCableNotificationsOptions[broadcasting]
+
+      results = model.
+                  select(options[:select]).
+                  limit(options[:limit]).
+                  where(options[:where]).
+                  scoped_collection(broadcasting_options[:scope]).
+                  to_a() rescue []
+
+      response = { collection: model.model_name.collection,
+        msg: 'update_many',
+        data: results
+      }
+
+      # Send data to the client
+      transmit response
     end
+
+    #
+    # Update one record from the DB
+    #
+    def update
+      # XXX: Check if the client is allowed to call the method
+
+      # Remove action name from data
+      data.except!("action")
+
+      # Record fetchnig options
+      options = {
+        id: data["id"],
+        fields: data["fields"]
+      }
+
+      model = self.stream_notification_options[:model]
+      broadcasting = self.stream_notification_options[:broadcasting]
+      broadcasting_options = model.ActionCableNotificationsOptions[broadcasting]
+
+      record = model.find(options[:id]) rescue nil
+
+      if record.present?
+        result = record.update_attributes(options[:fields]) rescue false
+
+        if !result
+          response = { collection: model.model_name.collection,
+            msg: 'error',
+            cmd: 'update',
+            error: record.errors.full_messages
+          }
+
+          # Send error notification to the client
+          transmit response
+        end
+      end
+    end
+
+    #
+    # Remove records from the DB
+    #
+    def remove
+
+    end
+
+    #
+    # Private methods
+    ################################################################
 
     private
 
