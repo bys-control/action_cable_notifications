deefa39838fde8c89c1694761b7671028310fb88
Guillermo Bisheimer <gbisheimer@bys-control.com.ar>
2017-05-04 16:15:25 -0300

    Add gem annotate in example app

--- examples/todo-vuejs/app/models/todo.rb
+++ examples/todo-vuejs/app/models/todo.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: todos
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  completed  :boolean
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Todo < ApplicationRecord
 end
